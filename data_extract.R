3497e49a7ab9c153f1e9bee0f9fcdbab6c8f32af
Bob Taylor <j.boaby@gmail.com>
2019-05-20 18:02:42 +0100

    initial commit

---
+++ data_extract.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+
+df <- read_csv("data/scotpho_data_extract.csv") %>% filter(area_type != "NA")
+
+area_names <- list()
+for(type in unique(df$area_type)){
+  names <- df %>% filter(area_type == type) %>%
+    select(area_name) %>%
+    distinct()
+  area_names[[type]] <- names
+}
+View(area_names)
+
+# Following are relevant area names:
+# "Alcohol & drug partnership": "North Ayrshire"
+# "Health board": "Ayrshire & Arran"
+# "Council area": "North Ayrshire"
+# "Intermediate zone": "Kilbirnie North", "Kilbirnie South & Longbar"
+# "HSC partnership": "North Ayrshire"
+# "HSC locality": "Garnock Valley"
+
+df_comparisons <- df %>%
+  filter(area_name %in% c("Scotland", "North Ayrshire", "Ayrshire & Arran", "Kilbirnie North", "Kilbirnie South & Longbar",
+                          "Garnock Valley")) %>%
+  select(-area_code)
+write_rds(df_comparisons, "data/data.rds", "bz")
