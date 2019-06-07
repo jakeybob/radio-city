f6e21cd6b3e568e870f868012c43e33275d70757
Bob Taylor <j.boaby@gmail.com>
2019-06-07 13:58:31 +0100

    faceted plot of main indicators

--- data_explore.R
+++ data_explore.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(ggplot2)
 library(plotly)
+library(RColorBrewer)
 
 # specific indicators to look into
 indicators <- c("Young people living in the most income deprived quintile",
@@ -18,9 +19,31 @@ df_rc <- dat %>% filter(area_name != "Scotland")
 #   filter(indicator %in% indicators)
 df <- df_rc %>% bind_rows(df_scot)
 
-p <- df %>% filter(indicator == indicators[2]) %>%
-  # filter(area_name == "Scotland") %>%
+df <- df %>% filter(indicator %in% indicators)
+
+normal_size <- 1
+scot_size <- 3
+line_sizes <- rep(normal_size, length(unique(paste0(df$area_name, df$area_type))))
+line_sizes[which(str_detect(unique(paste0(df$area_name, df$area_type)), "Scotland"))] <- scot_size
+
+p <- df %>% 
+  mutate(area_type = recode(area_type, Scotland = "National")) %>%
+  mutate(area_name = paste0(area_name, " (", area_type, ")")) %>%
+  mutate(indicator = paste0(indicator, ": ", definition)) %>%
   ggplot(aes(x = year, y = measure, color=area_name)) +
-  geom_point() +
-  geom_line()
-ggplotly(p)
+  geom_point(size = max(line_sizes) + 2) +
+  geom_line(aes(size = area_name)) +
+  scale_size_manual(values = line_sizes) +
+  # scale_color_brewer(palette = "Set1") +
+  # scale_color_manual(values=c("#7FC97F", "#AE9EC4", "#FDC086", "#DFDF79", "#F0027F", "#BF5B17", "#386CB0")) +
+  scale_color_manual(values=c("#E41A1C", "#A65628", "#4DAF4A", "#984EA3", "#FF7F00", "#DFDF13", "#276EA8")) +
+  facet_wrap(vars(indicator), scales = "free_y", labeller=label_wrap_gen(width = 50, multi_line = TRUE), nrow = 2) +
+  theme_bw() + 
+  xlab("") + ylab("") + 
+  theme(legend.title = element_blank(),
+        strip.background = element_rect(fill = "grey95"),
+        strip.text = element_text(face = "bold"))
+  
+p
+ggsave(filename = "pics/plot.pdf", device="pdf", width = 297, height = 210, units = "mm")
+# ggplotly(p)
